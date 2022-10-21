d2dd6d2f0c372374ace0d81a6c490c2f754e921a
sunjinwei <sunjinwei5828@163.com>
2022-10-21 09:03:11 +0800

    netty：如何启动服务端，最小化配置

--- netty_server/src/main/java/com/sunjinwei/netty/NettyServer.java
+++ netty_server/src/main/java/com/sunjinwei/netty/NettyServer.java
@@ -18,11 +18,19 @@ import io.netty.handler.codec.string.StringDecoder;
 public class NettyServer {
 
     public static void main(String[] args) {
+        // 最小化参数配置：线程模型+IO模型+连接读写逻辑处理
+
+        // 引导类：将引导服务端的启动工作
         ServerBootstrap serverBootstrap = new ServerBootstrap();
+        // boss指老板 worker指员工 一个老板负责从外面接活 很多员工负责具体干活
+        // 表示监听端口 接收新连接的线程组
         NioEventLoopGroup boss = new NioEventLoopGroup();
+        // 表示处理每一个连接的数据读写的线程组
         NioEventLoopGroup worker = new NioEventLoopGroup();
         serverBootstrap
+                // 给引导类配置两大线程组 这个引导类的线程模型也就定型类
                 .group(boss, worker)
+                // 指定服务端的IO模型为为NIO，如果想指定BIO 那么就是OioServerSocketChannel.class
                 .channel(NioServerSocketChannel.class)
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
@@ -36,6 +44,7 @@ public class NettyServer {
                         });
                     }
                 })
+                // 本地绑定一个8000端口
                 .bind(8000);
 
     }
